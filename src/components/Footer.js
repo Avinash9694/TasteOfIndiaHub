2325a894c9ef2f409a14c7270049a91f959f3164
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2023-12-14 16:43:35 +0530

    added footer section
    added footer section and made the home page more responsive

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer container">
+      <div className="footer-section">
+        <p className="title">FoodiesHub.com</p>
+        <p>
+          Taste of India Hub is a place where you can please your soul and tummy
+          with delicious food recepies of all cuisine.
+        </p>
+        <p>&copy; 2023 | All Rights Reserved</p>
+      </div>
+      <div className="footer-section">
+        <p className="title">Contact Us</p>
+        <p>tasteofindiahub@gmail.com</p>
+        <p>+91 999-999-9999</p>
+        <p>1234 street New Delhi</p>
+      </div>
+      <div className="footer-section">
+        <p className="title">Socials</p>
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
